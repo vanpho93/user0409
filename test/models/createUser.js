4dc423e92e4fca35413b5ec0fb5e401b40e80bd2
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-20 18:56:45 +0700

    create hash.js

--- test/models/createUser.js
+++ test/models/createUser.js
@@ -24,4 +24,23 @@ describe('Create user', () => {
             assert.equal(err.name, 'ValidationError');
         }
     });
+
+    it('Cannot create 2 user with only 1 email', async () => {
+        const user1 = new User({
+            email: 'b@gmail.com',
+            password: '123456',
+            name: 'abcd'
+        });
+        const user2 = new User({
+            email: 'b@gmail.com',
+            password: '654321',
+            name: 'efgh'
+        });
+        try {
+            await user1.save();
+            await user2.save();
+        } catch (err) {
+            assert.equal(err.name, 'MongoError');
+        }
+    });
 });
