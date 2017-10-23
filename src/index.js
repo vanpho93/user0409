3d45ad9e4677844117657f778a0105fff6cd4b0e
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-23 18:10:32 +0700

    encrypted

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { hash } = require('bcrypt');
+
 require('./db');
 const parser = require('body-parser').urlencoded({ extended: false });
 const User = require('./user');
@@ -10,8 +12,12 @@ app.set('views', './views');
 app.get('/dangky', (req, res) => res.render('dangky'));
 
 app.post('/dangky', parser, (req, res) => {
-    const user = new User(req.body);
-    user.save()
+    const { email, password, name } = req.body;
+    hash(password, 8)
+    .then(encrypted => {
+        const user = new User({ email, name, password: encrypted });
+        return user.save()
+    })
     .then(() => res.send('Dang ky thanh cong'))
     .catch((err) => res.send('Dang ky that bai. ' + err.message ));
 });
