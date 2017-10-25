8c18ed977bfe102db3165c8df1bb8771e6e9b657
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-25 17:50:03 +0700

    init

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('express-session');
+const app = express();
+
+app.use(session({
+    secret: 'asd823e8uwuq23',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 100000000 }
+}));
+
+app.get('/muave', (req, res) => {
+    req.session.daMuaVe = true;
+    res.send('Ban da mua ve');
+});
+
+app.get('/vaorap', (req, res) => {
+    console.log(req.session.daMuaVe);
+    if (!req.session.daMuaVe) return res.send('Ban phai mua ve truoc');
+    res.send('Moi xem phim');
+});
+
+
+app.listen(3000, () => console.log('Server started!'));
