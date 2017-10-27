8ebd2ceb83584ab7efee145b5c528742b60393d6
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-27 18:03:26 +0700

    test

---
+++ src/jwt.js
@@ -0,0 +1,7 @@
+function createToken(obj) {
+//return Promise cua token
+}
+
+function verifyToken(token) {
+//return Promise cua obj
+}
--- test/lib/jwt.js
+++ test/lib/jwt.js
@@ -1,12 +1,27 @@
 const assert = require('assert');
 const jwt = require('jsonwebtoken');
+const KEY = 'i1g81ewus12jw812uwu18f2';
 
 describe.only('Use jwt', () => {
-    it('Create token', () => {
-
+    xit('Create token', () => {
+        jwt.sign({ email:'vanpho01@gmail.com' }, KEY, (err, token) => {
+            console.log(token);
+        });
     });
 
     it('Verify token', () => {
-
+        const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InZhbnBobzAxQGdtYWlsLmNvbSIsImlhdCI6MTUwOTEwMTIwOX0.yGhYpidsUZRb-R8g3jf9-2MOUZxDPCGsPJnhXtV0H1c';
+        jwt.verify(token, KEY, (err, obj) => {
+            if (err) return console.log(err);
+            console.log(obj);
+        });
     });
 });
+
+/*
+
+// vanpho01@gmail.com + maHoaXyz
+// id
+
+*/
+
